f6261c4b30ff900aa73d733d4d427270c3ef87d7
David Harlow <david.harlow@thetin.net>
2020-04-09 12:21:57 +0100

    setup

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,8 +11,7 @@
     </p>
 
     <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <a href="https://davidharlow.dev/" target="_blank" rel="noopener">Old Site</a>
     </p>
 
   </Layout>
@@ -21,7 +20,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'David Harlow'
   }
 }
 </script>
