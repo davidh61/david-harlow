698732e2911ef4b688756756a16a3fd47ca914a7
David Harlow <david.harlow@thetin.net>
2020-04-23 22:06:59 +0100

    layout sorted

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="layout">
     <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
+      <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+      <h1>Hello World</h1>
+
       <nav class="nav">
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <!-- <g-link class="nav__link" to="/about-me/">About Me</g-link>
+        <g-link class="nav__link" to="/about/">Blogs</g-link> -->
       </nav>
     </header>
-    <slot/>
+    <div class="content">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -20,27 +24,37 @@ query {
 }
 </static-query>
 
-<style layout="scss">
+<style lang="scss">
 body {
-  font-family: $font-primary,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
 .layout {
+  padding-top: 20px;
   max-width: 760px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
 }
 
+.content {
+}
 .header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  text-align: center;
+  padding-right: 20px;
+}
+
+@media screen and (min-width: 1200px) {
+  .header {
+    float: left;
+  }
+}
+@media screen and (max-width: 1000px) {
+  .header {
+    display: block;
+  }
 }
 
 .nav__link {
