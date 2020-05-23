6bea3eb032378cb4c147375bf7058164f695780e
David Harlow <david.harlow@thetin.net>
2020-05-23 13:32:09 +0100

    toggled

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <button role="button" aria-label="Toggle dark/light" @click.prevent="toggleTheme" class="toggle-theme">
+      <p v-if="darkTheme">Dark</p>
+      <p v-else>Light</p>
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      darkTheme: false
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.darkTheme = !this.darkTheme
+      // This is using a script that is added in index.html
+      window.__setPreferredTheme(
+        this.darkTheme ? 'dark' : 'light'
+      )
+    }
+  },
+  mounted() {
+    if (window.__theme == 'dark') this.darkTheme = true
+  }
+}
+</script>
